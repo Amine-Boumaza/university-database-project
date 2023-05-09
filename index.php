0ea0cb6d18e13d15d98a7ef0735e21d0fb57958c
Mohamed Lamine <boumaza951@gmail.com>
2023-05-10 00:53:58 +0100

    commit

--- index.php
+++ index.php
@@ -7,10 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap"
-        rel="stylesheet">
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"
-        type='text/css'>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet" type='text/css'>
     <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
@@ -34,7 +32,7 @@
 
             <div class="list">
                 <h4>add a new researcher?</h4>
-                <a href="" class="show">ADD</a>
+                <a href="insert.php" class="show">INSERT</a>
             </div>
 
 
